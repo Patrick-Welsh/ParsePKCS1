901d7921d4ce9a11191d8acf0c580e3e1b21c6ce
Patrick Welsh <patrick.welsh@n-able.com>
2022-12-12 15:30:45 +0000

    Create main.go

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("**************************\nParsePKCS1 RSA Private Key\n**************************")
+	fmt.Println("\nThis script decodes a PEM block from a generated RSA key in PKCS1 format, and then parses the key")
+
+	fmt.Println("\nReading RSA key from file....")
+	pubPEMData, _ := os.ReadFile("rsa")
+
+	fmt.Println("\nDecoding PEM block....")
+	keyPem, _ := pem.Decode(pubPEMData)
+	key, err := x509.ParsePKCS1PrivateKey(keyPem.Bytes)
+
+	if err != nil {
+		fmt.Println("Could not parse RSA private key")
+	}
